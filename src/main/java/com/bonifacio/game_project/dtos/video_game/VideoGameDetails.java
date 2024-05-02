54513e4f5b3c71a7e5b13fd5a7a59a90c89469b3
Bonifacio Juarez Ceja <bonijuarezceja96@outlook.com>
2024-05-01 22:44:42 -0600

    creation the dtos deteils in video game

---
+++ src/main/java/com/bonifacio/game_project/dtos/video_game/VideoGameDetails.java
@@ -0,0 +1,25 @@
+package com.bonifacio.game_project.dtos.video_game;
+
+import com.bonifacio.game_project.dtos.classification.ClassificationOutDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import java.util.List;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@AllArgsConstructor
+@Getter
+@Builder
+public class VideoGameDetails {
+    private final UUID id;
+    private final String name;
+    private final String description;
+    private final String image;
+    private final LocalDate realiseDate;
+    private final Instant createAt;
+    private final Instant updateAt;
+    private final List<ClassificationOutDto> classifications;
+}
--- src/main/java/com/bonifacio/game_project/services/video_game/VideoGameService.java
+++ src/main/java/com/bonifacio/game_project/services/video_game/VideoGameService.java
@@ -1,12 +1,15 @@
 package com.bonifacio.game_project.services.video_game;
 
+import com.bonifacio.game_project.dtos.video_game.VideoGameDetails;
 import com.bonifacio.game_project.dtos.video_game.VideoGameInDto;
 import com.bonifacio.game_project.dtos.video_game.VideoGameOutDto;
 import com.bonifacio.game_project.entities.VideoGame;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface VideoGameService {
     List<VideoGameOutDto> findAll();
     VideoGame save(VideoGameInDto videoGameInDto);
+    VideoGameDetails show(UUID id);
 }
